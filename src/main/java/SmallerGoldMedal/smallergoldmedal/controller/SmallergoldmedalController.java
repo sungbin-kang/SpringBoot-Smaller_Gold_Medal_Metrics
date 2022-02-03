3141f1c13d3324c7649c244a06e3c0cf72b4072c
Sungbin_Kang <sungbin9353@gmail.com>
2022-02-03 08:55:21 -0800

    error: countires.html not compiling correctly

--- src/main/java/SmallerGoldMedal/smallergoldmedal/controller/SmallergoldmedalController.java
+++ src/main/java/SmallerGoldMedal/smallergoldmedal/controller/SmallergoldmedalController.java
@@ -3,12 +3,14 @@ package SmallerGoldMedal.smallergoldmedal.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
 import SmallerGoldMedal.smallergoldmedal.repositories.CountryRepository;
 import SmallerGoldMedal.smallergoldmedal.repositories.GoldMedalRepository;
 
 import SmallerGoldMedal.smallergoldmedal.model.Country;
 import SmallerGoldMedal.smallergoldmedal.model.GoldMedal;
+
 
 @RestController
 public class SmallergoldmedalController {
@@ -24,6 +26,14 @@ public class SmallergoldmedalController {
     @GetMapping("/countries")
     public Iterable<Country> getCountries() {
         return countryRepository.findAll();
+    }
+
+    @GetMapping("/countries")
+    public String getCountries(Model model) {
+        Iterable<Country> countries = countryRepository.findAll();
+        model.addAttribute("countries", countries);
+        
+        return "countries";
     }
 
 }
