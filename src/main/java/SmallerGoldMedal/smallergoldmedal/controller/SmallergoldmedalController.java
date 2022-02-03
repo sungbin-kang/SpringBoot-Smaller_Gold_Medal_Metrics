3867ba348a1cb72b11093e52829fc28e1061a28d
Sungbin_Kang <sungbin9353@gmail.com>
2022-02-03 08:19:28 -0800

    pass: repositories added to the controller

--- src/main/java/SmallerGoldMedal/smallergoldmedal/controller/SmallergoldmedalController.java
+++ src/main/java/SmallerGoldMedal/smallergoldmedal/controller/SmallergoldmedalController.java
@@ -4,11 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import SmallerGoldMedal.smallergoldmedal.repositories.CountryRepository;
+import SmallerGoldMedal.smallergoldmedal.repositories.GoldMedalRepository;
+
 @RestController
-// @RequestMapping("/countries")
 public class SmallergoldmedalController {
+
+    private final CountryRepository countryRepository;
+    private final GoldMedalRepository goldmedalRepository;
     
-    public SmallergoldmedalController() {
+    public SmallergoldmedalController(final CountryRepository countryRepository, final GoldMedalRepository goldmedalRepository) {
+        this.countryRepository = countryRepository;
+        this.goldmedalRepository = goldmedalRepository;
     }
 
     @GetMapping("/test")
